28f20e7946643ea349e356f470afb79aa804627f
Kim Myungseong <forwarm5891@gmail.com>
2022-04-28 05:13:53 +0900

    design : re-design global,DetailLayout design

--- components/assets/Layouts/DetailLayout.tsx
+++ components/assets/Layouts/DetailLayout.tsx
@@ -1,23 +1,25 @@
 import styled from '@emotion/styled';
 import Link from 'next/link';
 import React, { ReactNode } from 'react';
-
-
+import router, { useRouter } from 'next/router'
+import Button from '../Button'
+import { ButtonWrapper } from '../../Styles';
 interface IDetailLayoutProps {
   children: ReactNode
 }
 
 
+
 const DetailLayout = ({children}:IDetailLayoutProps) => {
+  const router = useRouter()
   return (
     <Layout>
 
       {children}
       
-
-      <Link href="/">
-      <a>뒤로가기</a>
-      </Link>
+      <ButtonWrapper>
+      <Button onClick={()=>router.back()}>뒤로가기</Button>
+      </ButtonWrapper>
     </Layout>
   );
 };
@@ -26,5 +28,7 @@ const DetailLayout = ({children}:IDetailLayoutProps) => {
 const Layout = styled.div`
   padding: 10rem 2rem;
 `
+
+
 
 export default DetailLayout;
