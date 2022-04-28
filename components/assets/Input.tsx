16c04db235f4e537737ff52e21cedd6668a4ef3a
Kim Myungseong <forwarm5891@gmail.com>
2022-04-28 12:09:08 +0900

    refactoring: refactoring searchInput func

--- components/assets/Input.tsx
+++ components/assets/Input.tsx
@@ -3,22 +3,43 @@ import React, { ChangeEvent, FormEvent, useState } from 'react';
 import SearchSVG from './svg/SearchSVG';
 
 interface IInputProps {
-  [key: string]: any;
+  handleInputValue: (e: React.KeyboardEvent<HTMLInputElement>) => void
+  handleSearch: (e:React.MouseEvent<HTMLButtonElement>) => void
+  handleSearchValue:(e: React.ChangeEvent<HTMLInputElement>) => void
+  searchInputValue:string;
 }
 
-const Input = ({ ...props }: IInputProps) => {
+const Input = ({handleSearchValue,handleInputValue,handleSearch,searchInputValue }: IInputProps) => {
+  const searchValue = searchInputValue;
   return (
     <>
       <SearchForm>
         <SearchWrapper>
           <SearchSVG />
         </SearchWrapper>
-        <MainInput onKeyUp={props.handleInputValue} />
-        <SubmitInput>찾기</SubmitInput>
+        <MainInput name='searchInput' type="text" value={searchValue||''} placeholder="우리동네 착한가게"onChange={handleSearchValue} onKeyPress={handleInputValue} />
+        <SubmitButton onClick={handleSearch}>찾기</SubmitButton>
       </SearchForm>
     </>
   );
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const SearchForm = styled.div`
   position: relative;
@@ -41,16 +62,30 @@ const MainInput = styled.input`
     box-shadow: 5px 5px 20px #a13111;
   }
 `;
-const SubmitInput = styled.button`
+const SubmitButton = styled.button`
   position: absolute;
   right: 140px;
   border: 0;
   background-color: transparent;
   font-size: 12px;
   cursor: pointer;
-  &:hover {
-    background-color: rgba(51, 51, 255, 0.2);
-    border-radius: 20px;
+  
+  &::before{
+    content:'';
+    position:absolute;
+    top:-7px;
+    width:25px;
+    height:25px;
+    left:-4px;
+    transition:.2s;
+    border-radius:50%;
+    padding:.2rem;
+  }
+  &:hover:before{
+    
+    
+    background-color: rgba(187, 34, 51,0.2);
+
   }
 `;
 
